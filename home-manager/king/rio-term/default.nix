27b077a85aff5dbce67e484201f9f2e30dc69efc
tkmockingbird <zbellpeppers@pm.me>
2025-02-12 09:25:57 -0500

    Changed rio theme

--- home-manager/king/rio-term/default.nix
+++ home-manager/king/rio-term/default.nix
@@ -2,7 +2,7 @@
   programs.rio = {
     enable = true;
     settings = {
-      theme = "/home/zachary/.config/themes/Breeze.toml";
+      theme = "/home/zachary/.config/themes/rio-term/OneHalfDark.toml";
       hide-cursor-when-typing = true;
       padding-x = 8;
       padding-y = [
